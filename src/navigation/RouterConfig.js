77ac398e387803c43f9c5a743fa46555dd925065
Ali Hassan <ali.hassan@magnusmage.com>
2020-12-31 14:08:20 +0500

    reset dorectory structure

--- src/navigation/RouterConfig.js
+++ src/navigation/RouterConfig.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-
-import landing from '../views/landing/Landing';
-
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Layout from '../components/layout';
-// import '../styles/css/index.css';
-// import 'react-circular-progressbar/dist/styles.css';
-
-// import Searchbox from "./HomeExplorer/components/SearchBox";
+import { Layout } from 'src/layouts';
+import { Landing } from 'src/views/landing';
+import { HOME } from './CONSTANTS';
 
 class App extends Component {
   render() {
@@ -15,7 +10,7 @@ class App extends Component {
       <Router>
         <Layout>
           <Switch>
-            <Route exact path="/" component={landing} />
+            <Route exact path={HOME} component={Landing} />
           </Switch>
         </Layout>
       </Router>

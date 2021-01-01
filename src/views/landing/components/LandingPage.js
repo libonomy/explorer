497ca0835c05c7debbe6a139ea7bdbe01264fbd7
jibran baig <jibran.baig@magnusmage.com>
2021-01-01 17:05:09 +0500

    footer done

--- src/views/landing/components/LandingPage.js
+++ src/views/landing/components/LandingPage.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import { Card, CardText, CardTitle, Col, Container } from 'reactstrap';
 const LandingPage = () => {
-  return <div>ali</div>;
+  return (
+    <Container>
+      <Col lg="6" md="6" sm="6">
+        <Card>
+          <div className="cards-body">
+            <div className="body-content">
+              <div className="body-inner-div">
+                <span className="img-div">{/* <img src={marketCap} /> */}</span>
+                <CardTitle>Circulation Amount</CardTitle>
+              </div>
+              <div>
+                <CardText>N/A</CardText>
+              </div>
+            </div>
+            <div className="body-content">
+              <div className="body-inner-div">
+                <span className="img-div"></span>
+                <CardTitle>Active Amount</CardTitle>
+              </div>
+              <div>
+                <CardText>N/A</CardText>
+              </div>
+            </div>
+          </div>
+        </Card>
+      </Col>
+    </Container>
+  );
 };
 
 export default LandingPage;

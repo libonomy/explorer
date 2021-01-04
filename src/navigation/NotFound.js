a1c4660e9a185a672531d8283c1750de24cb27af
Ali Hassan <ali.hassan@magnusmage.com>
2021-01-04 16:54:27 +0500

    page not found styling

--- src/navigation/NotFound.js
+++ src/navigation/NotFound.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'reactstrap';
 import styled from 'styled-components';
 import { fourOFour, wireBreak } from 'src/assets/images';
+import colors from 'src/vars/colors';
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
@@ -13,13 +14,18 @@ const FourOFour = styled.img``;
 const WireBreak = styled.img`
   max-width: 100%;
 `;
-const Content = styled.p``;
+const Content = styled.p`
+  font-size: 24px;
+  color: ${colors.primary};
+  line-height: 4;
+  font-family: PoppinsBold;
+`;
 const NotFound = () => {
   return (
     <Container>
       <Wrapper>
         <FourOFour src={fourOFour} />
-        <Content>Whoops page not found</Content>
+        <Content>Whoops ... page not found :(</Content>
         <WireBreak src={wireBreak} />
       </Wrapper>
     </Container>
